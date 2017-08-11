ff35d7a67461875f5b9fa07b8d8fd46fd910eb1a
AFlyingGrayson <theflyinggrayson01@gmail.com>
2017-08-11 11:31:54 -0500

    fixed resources, made small edits, prepared for alpha release - no longer has rpmod resources - abilities are now named - added status messages to tell player what is going on - can now only regenerate 12 times (13 total lives)

--- src/main/java/com/afg/regeneration/superpower/TimelordHandler.java
+++ src/main/java/com/afg/regeneration/superpower/TimelordHandler.java
@@ -9,7 +9,7 @@ import lucraft.mods.lucraftcore.util.LucraftCoreUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -45,6 +45,8 @@ public class TimelordHandler extends SuperpowerPlayerHandler
 			player.setHealth(player.getMaxHealth());
 			randomizeTraits(this);
 			this.regenCount++;
+			Minecraft.getMinecraft().gameSettings.thirdPersonView = 0;
+
 		}
 	}
 
@@ -65,6 +67,20 @@ public class TimelordHandler extends SuperpowerPlayerHandler
 
 		LucraftCoreUtil.sendSuperpowerUpdatePacketToAllPlayers(handler.player);
 
+		String s = "";
+		for (Ability ability : handler.getAbilities())
+		{
+			if(ability.isUnlocked())
+			{
+				if (s.equals(""))
+					s = ability.getDisplayName().substring(7);
+				else
+					s = s + ", " + ability.getDisplayName().substring(7);
+			}
+		}
+		s = s + ".";
+
+		handler.player.sendStatusMessage(new TextComponentString("You've gotten a new life, with new traits: " + s), true);
 	}
 
 	@Override public NBTTagCompound writeToNBT(NBTTagCompound compound)
@@ -82,20 +98,6 @@ public class TimelordHandler extends SuperpowerPlayerHandler
 		regenTicks = compound.getInteger("regenTicks");
 	}
 
-
-	@SubscribeEvent
-	public static void onDeath(LivingDeathEvent e){
-		if(e.getEntity() instanceof EntityPlayer){
-			EntityPlayer player = (EntityPlayer) e.getEntity();
-			if(SuperpowerHandler.hasSuperpower(player, Regeneration.timelord)){
-				TimelordHandler handler = SuperpowerHandler.getSpecificSuperpowerPlayerHandler(player, TimelordHandler.class);
-				e.setCanceled(true);
-				if(handler.regenTicks == 0)
-					handler.regenTicks = 1;
-			}
-		}
-	}
-
 	@SubscribeEvent
 	public static void onHurt(LivingHurtEvent e){
 		if(e.getEntity() instanceof EntityPlayer){
@@ -111,11 +113,30 @@ public class TimelordHandler extends SuperpowerPlayerHandler
 
 				if(((EntityPlayer) e.getEntity()).getHealth() - e.getAmount() <= 0)
 				{
-					e.setCanceled(true);
-					((EntityPlayer) e.getEntity()).setHealth(0.1f);
-					if(handler.regenTicks == 0)
-						handler.regenTicks = 1;
-					LucraftCoreUtil.sendSuperpowerUpdatePacketToAllPlayers(player);
+					if(handler.regenCount < 12)
+					{
+						e.setCanceled(true);
+						((EntityPlayer) e.getEntity()).setHealth(0.1f);
+						if (handler.regenTicks == 0)
+							handler.regenTicks = 1;
+						LucraftCoreUtil.sendSuperpowerUpdatePacketToAllPlayers(player);
+
+						String time = "" + (handler.regenCount + 1);
+						switch (handler.regenCount + 1){
+						case 1: time = time + "st";
+							break;
+						case 2: time = time + "nd";
+							break;
+						case 3: time = time + "rd";
+							break;
+						default: time = time + "th";
+							break;
+						}
+						handler.player.sendStatusMessage(new TextComponentString("You're regenerating for the " + time + " time, you have " + (11 - handler.regenCount) + " regenerations left."), true);
+
+					} else {
+						handler.player.sendStatusMessage(new TextComponentString("You're out of regenerations. You're dying for real this time."), true);
+					}
 				}
 
 			}
