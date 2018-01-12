a22bb4b2d26ab261081757c41453b37840c6206a
AFlyingGrayson <theflyinggrayson01@gmail.com>
2018-01-11 23:13:43 -0600

    Changed limb rotation method to most current, from LC dev branch.
    - Should ease porting later, and clean up render code now.

--- src/main/java/com/lcm/regeneration/client/animation/LayerRegenerationLimbs.java
+++ src/main/java/com/lcm/regeneration/client/animation/LayerRegenerationLimbs.java
@@ -1,10 +1,8 @@
 package com.lcm.regeneration.client.animation;
 
-import com.lcm.regeneration.superpower.TimelordSuperpower;
 import com.lcm.regeneration.superpower.TimelordSuperpowerHandler;
 import lucraft.mods.lucraftcore.superpowers.SuperpowerHandler;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
@@ -13,7 +11,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.awt.*;
+import javax.annotation.Nonnull;
 
 /**
  * Created by AFlyingGrayson on 8/8/17
@@ -24,58 +22,37 @@ public class LayerRegenerationLimbs implements LayerRenderer<EntityPlayer> {
 	private static final ModelPlayer playerModelLargeArms = new ModelPlayer(0.55F, false);
 	private static final ModelPlayer playerModelSmallArms = new ModelPlayer(0.55F, true);
 	
-	public LayerRegenerationLimbs(RenderPlayer playerRenderer) {
-		this.playerRenderer = playerRenderer;
-	}
+	LayerRegenerationLimbs(RenderPlayer playerRenderer) { this.playerRenderer = playerRenderer; }
 	
 	@Override
-	public void doRenderLayer(EntityPlayer player, float p_177169_2_, float p_177169_3_, float p_177169_4_, float p_177169_5_, float p_177169_6_, float p_177169_7_, float p_177169_8_) {
-		if (!SuperpowerHandler.hasSuperpower(player, TimelordSuperpower.INSTANCE)) return;
-		
+	public void doRenderLayer(@Nonnull EntityPlayer player, float p_177169_2_, float p_177169_3_, float p_177169_4_, float p_177169_5_, float p_177169_6_, float p_177169_7_, float p_177169_8_) {
 		TimelordSuperpowerHandler handler = SuperpowerHandler.getSpecificSuperpowerPlayerHandler(player, TimelordSuperpowerHandler.class);
-		if (!(handler.regenTicks > 0 && handler.regenTicks < 200)) return;
-		
-		Color color = new Color(255, 200, 0, 1);
-		
-		boolean smallArms = ((AbstractClientPlayer) player).getSkinType().equals("slim");
-		ModelPlayer playerModel = smallArms ? LayerRegenerationLimbs.playerModelSmallArms : LayerRegenerationLimbs.playerModelLargeArms;
-		
+		if (handler == null || !(handler.regenTicks > 0 && handler.regenTicks < 200)) return;
+
+		ModelPlayer playerModel = ((AbstractClientPlayer) player).getSkinType().equals("slim") ? LayerRegenerationLimbs.playerModelSmallArms : LayerRegenerationLimbs.playerModelLargeArms;
+
+		if (handler.regenTicks > 150) {
+			playerModel.bipedBody.isHidden = playerModel.bipedLeftLeg.isHidden = playerModel.bipedRightLeg.isHidden = playerModel.bipedBodyWear.isHidden = playerModel.bipedHeadwear.isHidden = playerModel.bipedLeftLegwear.isHidden = playerModel.bipedRightLegwear.isHidden = false;
+		} else {
+			playerModel.bipedBody.isHidden = playerModel.bipedLeftLeg.isHidden = playerModel.bipedRightLeg.isHidden = playerModel.bipedBodyWear.isHidden = playerModel.bipedHeadwear.isHidden = playerModel.bipedLeftLegwear.isHidden = playerModel.bipedRightLegwear.isHidden = true;
+		}
+
 		GlStateManager.pushAttrib();
 		GlStateManager.disableTexture2D();
-		GlStateManager.color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
+		GlStateManager.color(255, 200, 0,1);
 		GlStateManager.enableAlpha();
 		GlStateManager.enableBlend();
 		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
-		
-		// Left Arm/Armwear
-		LimbRotationUtil.createCustomModelRenderer(playerModel, 0, 0, -75, ModelBiped.class.getDeclaredFields()[4]);
-		LimbRotationUtil.createCustomModelRenderer(playerModel, 0, 0, -75, ModelPlayer.class.getDeclaredFields()[0]);
-		
-		// Right Arm/Armwear
-		LimbRotationUtil.createCustomModelRenderer(playerModel, 0, 0, 75, ModelBiped.class.getDeclaredFields()[3]);
-		LimbRotationUtil.createCustomModelRenderer(playerModel, 0, 0, 75, ModelPlayer.class.getDeclaredFields()[1]);
-		
-		// Head/Headwear
-		LimbRotationUtil.createCustomModelRenderer(playerModel, -20, 0, 0, ModelBiped.class.getDeclaredFields()[0]);
-		LimbRotationUtil.createCustomModelRenderer(playerModel, -20, 0, 0, ModelBiped.class.getDeclaredFields()[1]);
-		
+
 		playerModel.setModelAttributes(playerRenderer.getMainModel());
 		playerModel.render(player, p_177169_2_, p_177169_3_, p_177169_5_, p_177169_6_, p_177169_7_, p_177169_8_);
-		
+
 		GlStateManager.disableAlpha();
 		GlStateManager.disableBlend();
 		GlStateManager.color(255, 255, 255, 255);
 		GlStateManager.enableTexture2D();
 		GlStateManager.popAttrib();
-		
-		if (handler.regenTicks > 150) {
-			LayerRegenerationLimbs.playerModelSmallArms.bipedBody.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedLeftLeg.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedRightLeg.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedBodyWear.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedHeadwear.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedLeftLegwear.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedRightLegwear.isHidden = false;
-			LayerRegenerationLimbs.playerModelLargeArms.bipedBody.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedLeftLeg.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedRightLeg.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedBodyWear.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedHeadwear.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedLeftLegwear.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedRightLegwear.isHidden = false;
-		} else {
-			LayerRegenerationLimbs.playerModelSmallArms.bipedBody.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedLeftLeg.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedRightLeg.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedBodyWear.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedHeadwear.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedLeftLegwear.isHidden = LayerRegenerationLimbs.playerModelSmallArms.bipedRightLegwear.isHidden = true;
-			LayerRegenerationLimbs.playerModelLargeArms.bipedBody.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedLeftLeg.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedRightLeg.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedBodyWear.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedHeadwear.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedLeftLegwear.isHidden = LayerRegenerationLimbs.playerModelLargeArms.bipedRightLegwear.isHidden = true;
-		}
 	}
-	
+
 	@Override public boolean shouldCombineTextures() { return false; }
 }
--- src/main/java/com/lcm/regeneration/client/animation/PlayerRenderHandler.java
+++ src/main/java/com/lcm/regeneration/client/animation/PlayerRenderHandler.java
@@ -1,11 +1,7 @@
 package com.lcm.regeneration.client.animation;
 
-import com.lcm.regeneration.superpower.TimelordSuperpower;
 import com.lcm.regeneration.superpower.TimelordSuperpowerHandler;
 import lucraft.mods.lucraftcore.superpowers.SuperpowerHandler;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.model.ModelPlayer;
-import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -20,38 +16,16 @@ public class PlayerRenderHandler {
 	
 	@SubscribeEvent
 	public void onRenderPlayerPre(RenderPlayerEvent.Pre e) {
-		if (!SuperpowerHandler.hasSuperpower(e.getEntityPlayer(), TimelordSuperpower.INSTANCE)) return;
-		
-		TimelordSuperpowerHandler handler = SuperpowerHandler.getSpecificSuperpowerPlayerHandler(e.getEntityPlayer(), TimelordSuperpowerHandler.class);
-		if (handler.regenTicks > 0 && handler.regenTicks < 200) {
-			ModelPlayer player = e.getRenderer().getMainModel();
-			
-			// Left Arm/Armwear
-			LimbRotationUtil.createCustomModelRenderer(player, 0, 0, -75, ModelBiped.class.getDeclaredFields()[4]);
-			LimbRotationUtil.createCustomModelRenderer(player, 0, 0, -75, ModelPlayer.class.getDeclaredFields()[0]);
-			
-			// Right Arm/Armwear
-			LimbRotationUtil.createCustomModelRenderer(player, 0, 0, 75, ModelBiped.class.getDeclaredFields()[3]);
-			LimbRotationUtil.createCustomModelRenderer(player, 0, 0, 75, ModelPlayer.class.getDeclaredFields()[1]);
-			
-			// Head/Headwear
-			LimbRotationUtil.createCustomModelRenderer(player, -20, 0, 0, ModelBiped.class.getDeclaredFields()[0]);
-			LimbRotationUtil.createCustomModelRenderer(player, -20, 0, 0, ModelBiped.class.getDeclaredFields()[1]);
-		}
-	}
-	
-	@SubscribeEvent
-	public void onRenderPlayerPost(RenderPlayerEvent.Post e) {
 		if (!layersAddedTo.contains(e.getEntityPlayer())) {
 			layersAddedTo.add(e.getEntityPlayer());
 			e.getRenderer().addLayer(new LayerRegenerationLimbs(e.getRenderer()));
 		}
-		
-		if (SuperpowerHandler.hasSuperpower(e.getEntityPlayer(), TimelordSuperpower.INSTANCE)) {
-			TimelordSuperpowerHandler handler = SuperpowerHandler.getSpecificSuperpowerPlayerHandler(e.getEntityPlayer(), TimelordSuperpowerHandler.class);
-			if (handler.regenTicks > 0) for (ModelRenderer renderer : e.getRenderer().getMainModel().boxList) {
-				if (renderer instanceof LimbRotationUtil.CustomModelRenderer) ((LimbRotationUtil.CustomModelRenderer) renderer).reset();
-			}
+
+		TimelordSuperpowerHandler handler = SuperpowerHandler.getSpecificSuperpowerPlayerHandler(e.getEntityPlayer(), TimelordSuperpowerHandler.class);
+		if (handler == null || handler.regenTicks > 0 && handler.regenTicks < 200) {
+			LimbManipulationUtil.getLimbManipulator(e.getRenderer(), LimbManipulationUtil.Limb.LEFT_ARM).setAngles(0, 0, -75);
+			LimbManipulationUtil.getLimbManipulator(e.getRenderer(), LimbManipulationUtil.Limb.RIGHT_ARM).setAngles(0, 0, 75);
+			LimbManipulationUtil.getLimbManipulator(e.getRenderer(), LimbManipulationUtil.Limb.HEAD).setAngles(-20, 0, 0);
 		}
 	}
 }
