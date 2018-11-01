88940c18dfbf78f0d49534808166846eb922f9fe
HoldYourWaffle <ravivanrooijen@live.nl>
2018-11-01 17:34:47 +0100

    Add language keys for configuration

--- src/main/java/me/fril/RegenConfig.java
+++ src/main/java/me/fril/RegenConfig.java
@@ -11,72 +11,71 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
  * on 17/09/2018.
  */
 @Config(modid = Regeneration.MODID)
-public class RegenConfig {
+public class RegenConfig { //TODO externalize comment strings?
 	
-	//public static final RegenConfig RegenTEST = new RegenConfig();
     public static final Loot Loot = new Loot();
 
-    @Config.LangKey("config.max_regens")
+    @Config.LangKey("config.regeneration.max_regens")
     @Config.Comment("The maximum regeneration capacity. This affects the durability of a Fob Watch and the amount of regenerations in a full cycle. Use 0 for infinite regenerations, the Fob Watch will grant the timelord ability and give you infinite regenerations. If you die while regenerating you'll lose your ability (unless dontLosePower is set to true)")
     @Config.RangeInt(min = 0)
     public static int regenCapacity = 12;
 
-    @Config.LangKey("config.start_as_timelord")
-    @Config.Comment("The maximum regeneration capacity. This affects the durability of a Fob Watch and the amount of regenerations in a full cycle. Use 0 for infinite regenerations, the Fob Watch will grant the timelord ability and give you infinite regenerations. If you die while regenerating you'll lose your ability (unless dontLosePower is set to true)")
+    @Config.LangKey("config.regeneration.start_as_timelord")
+    @Config.Comment("If true you'll start a world with a Fob Watch in your inventory")
     public static boolean startAsTimelord = false;
 
-    @Config.LangKey("config.fiery_regen")
+    @Config.LangKey("config.regeneration.fiery_regen")
     @Config.Comment("Spawn fire when regenerating")
     public static boolean fieryRegen = true;
 
-    @Config.LangKey("config.regenerative_kill_range")
+    @Config.LangKey("config.regeneration.regenerative_kill_range")
     @Config.Comment("Upon regeneration every mob inside of this radius is immediately killed. Keep in mind that you should round up to accommodate for mobs that aren't standing in the center of a block")
     @Config.RangeInt(max = 200, min = 0)
     public static int regenerativeKillRange = 4;
 
-    @Config.LangKey("config.regenerative_knockback")
+    @Config.LangKey("config.regeneration.regenerative_knockback")
     @Config.Comment("The amount of knockback every mob inside of the knock back radius gets")
     public static float regenerativeKnockback = 2.5F;
 
-    @Config.LangKey("config.regenerative_knockback_range")
+    @Config.LangKey("config.regeneration.regenerative_knockback_range")
     @Config.Comment("Range wherein every mob is knocked back upon regeneration")
     @Config.RangeInt(max = 300, min = 0)
     public static int regenerativeKnockbackRange = 7;
 
-    @Config.LangKey("config.post_regen_duration")
+    @Config.LangKey("config.regeneration.post_regen_duration")
     @Config.Comment("Amount of seconds the post-regeneration effect lasts")
     public static int postRegenerationDuration = 180;
 
-    @Config.LangKey("config.post_regenerationEffect_level")
-    @Config.Comment("The level of the regeneration status effect granted upon regeneration finish")
+    @Config.LangKey("config.regeneration.post_regenerationEffect_level")
+    @Config.Comment("The level of the regeneration status effect granted after you regenerate")
     public static int postRegenerationLevel = 4;
 
-    @Config.LangKey("config.reset_hunger")
+    @Config.LangKey("config.regeneration.reset_hunger")
     @Config.Comment("Regenerate hunger bars")
     public static boolean resetHunger = true;
 
-    @Config.LangKey("config.reset_oxygen")
+    @Config.LangKey("config.regeneration.reset_oxygen")
     @Config.Comment("Regenerate Oxygen")
     public static boolean resetOxygen = true;
 
-    @Config.LangKey("config.absorbtion_level")
+    @Config.LangKey("config.regeneration.absorbtion_level")
     @Config.Comment("The amount of absorption hearts you get when regenerating")
     public static int absorbtionLevel = 10;
 
-    @Config.LangKey("config.lose_regens_on_death")
-    @Config.Comment("If this is false you won't lose your timelord power if you get killed during regeneration")
+    @Config.LangKey("config.regeneration.lose_power_on_mid_regen_death")
+    @Config.Comment("If this is false you won't lose your regenerations if you get killed during regeneration")
     public static boolean losePowerOnMidRegenDeath = true;
     
     
 
-    @Config.LangKey("category.loot")
     public static class Loot {
-        @Config.LangKey("config.loot_regex")
+    	
+        @Config.LangKey("config.regeneration.loot_regex")
         @Config.Comment("The loot pool for chameleon arch's will only be added to loot tables whose name matches this regular expression")
         @Config.RequiresWorldRestart
         public String lootRegex = "minecraft:chests\\/.*";
 
-        @Config.LangKey("config.disable_loot")
+        @Config.LangKey("config.regeneration.disable_loot")
         @Config.Comment("If this is true there won't be any Fob Watches spawned naturally")
         @Config.RequiresWorldRestart
         public boolean disableLoot = false;
