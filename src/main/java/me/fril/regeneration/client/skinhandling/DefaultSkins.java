997177e8171db6872c78498d79c45a01585f6e3e
Suffril <sandedshoes@gmail.com>
2019-01-18 22:12:11 +0000

    New Skin urls

--- src/main/java/me/fril/regeneration/client/skinhandling/DefaultSkins.java
+++ src/main/java/me/fril/regeneration/client/skinhandling/DefaultSkins.java
@@ -3,6 +3,15 @@ package me.fril.regeneration.client.skinhandling;
 public enum DefaultSkins {
 	SIXTH(true, "https://github.com/Suffril/Regeneration/blob/skins/skins/alex/six_doctor.png?raw=true"),
 	ELEVENTH(true, "https://github.com/Suffril/Regeneration/blob/skins/alex/eleventh_doctor.png?raw=true"),
+	GORILLAZ(true, "https://github.com/Suffril/Regeneration/blob/skins/alex/gorillaz.png?raw=true"),
+	JERRY(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/jerry.png?raw=true"),
+	AGRENT(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/agent.png?raw=true"),
+	DOC_GREEN(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/doc_green.png?raw=true"),
+	FRENCH(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/the_french.png?raw=true"),
+	JULI(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/juli.png?raw=true"),
+	MARTY(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/marty.png?raw=true"),
+	THATGUY(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/thatguy.png?raw=true"),
+	ANCIENT_EMO(false, "https://github.com/Suffril/Regeneration/blob/skins/steve/ancient_emo.png?raw=true"),
 	HEROBRINE(false, "https://github.com/Suffril/Regeneration/blob/skins/skins/steve/herobrine.png?raw=true");
 	
 	private final String url;
