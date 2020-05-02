c66256a3fba5d8bc54a45d6b98375769e77a5c89
LotuxPunk <vandendaelenclement@gmail.com>
2020-05-02 16:18:51 +0200

    refactor commands

---
+++ src/main/java/me/swirtzly/regeneration/common/commands/RegenCommand.java
@@ -0,0 +1,26 @@
+package me.swirtzly.regeneration.common.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import me.swirtzly.regeneration.RegenerationMod;
+import me.swirtzly.regeneration.common.commands.subcommands.FastForwardCommand;
+import me.swirtzly.regeneration.common.commands.subcommands.GlowCommand;
+import me.swirtzly.regeneration.common.commands.subcommands.SetRegensCommand;
+import me.swirtzly.regeneration.common.commands.subcommands.SetTraitsCommand;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+
+public class RegenCommand {
+
+    public static void register(CommandDispatcher<CommandSource> dispatcher) {
+        dispatcher.register(
+                Commands.literal(RegenerationMod.MODID)
+                        .requires(commandSource -> commandSource.hasPermissionLevel(ServerLifecycleHooks.getCurrentServer().getOpPermissionLevel()))
+                            .then(SetRegensCommand.register(dispatcher))
+                            .then(GlowCommand.register(dispatcher))
+                            .then(FastForwardCommand.register(dispatcher))
+                            .then(SetTraitsCommand.register(dispatcher))
+        );
+
+    }
+}
