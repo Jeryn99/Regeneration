80a03729bdb16c48c28d5df3ceb1ccc7d4322710
Swirtzly <sandedshoes@gmail.com>
2020-06-10 22:02:52 +0100

    Fixes to ARS and localisation Zero room is no longer a compat block

--- src/main/java/me/swirtzly/regeneration/common/block/ZeroRoomBlock.java
+++ src/main/java/me/swirtzly/regeneration/common/block/ZeroRoomBlock.java
@@ -1,13 +1,12 @@
 package me.swirtzly.regeneration.common.block;
 
-import me.swirtzly.regeneration.util.common.ICompatObject;
 import net.minecraft.block.Block;
 
 /**
  * Created by Swirtzly
  * on 20/05/2020 @ 11:30
  */
-public class ZeroRoomBlock extends Block implements ICompatObject {
+public class ZeroRoomBlock extends Block {
     public ZeroRoomBlock(Properties properties) {
         super(properties);
     }
