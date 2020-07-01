7235b4e22a8cec741254764d774f5f4fdd2533b2
Swirtzly <sandedshoes@gmail.com>
2020-07-01 22:27:01 +0100

    Removed laggy world-gen code, cannot publish fix until tomorrow

--- src/main/java/me/swirtzly/regeneration/common/dimension/biomes/GallifrayanWastelands.java
+++ src/main/java/me/swirtzly/regeneration/common/dimension/biomes/GallifrayanWastelands.java
@@ -3,15 +3,9 @@ package me.swirtzly.regeneration.common.dimension.biomes;
 import me.swirtzly.regeneration.common.dimension.features.BiomeHelper;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.util.SharedSeedRandom;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.DefaultBiomeFeatures;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.GenerationSettings;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
@@ -50,49 +44,5 @@ public class GallifrayanWastelands extends Biome {
     @Override
     public int getFoliageColor(BlockPos pos) {
         return WASTELAND_LEAVES;
-    }
-
-    @Override
-    public void decorate(GenerationStage.Decoration stage, ChunkGenerator<? extends GenerationSettings> chunkGenerator, IWorld worldIn, long seed, SharedSeedRandom random, BlockPos pos) {
-        super.decorate(stage, chunkGenerator, worldIn, seed, random, pos);
-
-        int maxSandStone = 100;
-        for (int sandstone = 0; sandstone < maxSandStone; ++sandstone) {
-            BlockPos sstonePos = worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (pos.add(random.nextInt(16), 0, random.nextInt(16))));
-            if (worldIn.getBlockState(sstonePos.down()).getBlock() == Blocks.SAND) {
-                worldIn.setBlockState(sstonePos.down(), SANDSTONE, 7);
-            }
-        }
-
-        int maxDirt = 25;
-        for (int dirt = 0; dirt < maxDirt; ++dirt) {
-            BlockPos dirtPos = worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (pos.add(random.nextInt(16), 0, random.nextInt(16))));
-            if (worldIn.getBlockState(dirtPos.down()).getBlock() == Blocks.SAND) {
-                worldIn.setBlockState(dirtPos.down(), DIRT, 7);
-            }
-        }
-
-        int maxGrass = 16;
-        for (int grass = 0; grass < maxGrass; ++grass) {
-            BlockPos grassPos = worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (pos.add(random.nextInt(14), 0, random.nextInt(14))));
-            if (worldIn.getBlockState(grassPos.down()).getBlock() == Blocks.DIRT) {
-                worldIn.setBlockState(grassPos, GRASS, 7);
-            }
-        }
-
-
-        int maxSkull = 1;
-        for (int skull = 0; skull < maxSkull; ++skull) {
-
-            int percentageSpawn = random.nextInt(100);
-
-            if (percentageSpawn == 1) {
-
-                BlockPos skullPos = worldIn.getHeight(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (pos.add(random.nextInt(16), 0, random.nextInt(16))));
-                worldIn.setBlockState(skullPos, Blocks.SKELETON_SKULL.getDefaultState(), 7);
-            }
-        }
-
-
     }
 }
