15caef5067f11633f40d77daa35f2ea34aebff6a
Craig <sandedshoes@gmail.com>
2022-08-04 17:56:29 +0100

    Fxing lang

---
+++ src/main/java/craig/software/mc/regen/mixin/AbstractClientPlayerMixin.java
@@ -0,0 +1,27 @@
+package craig.software.mc.regen.mixin;
+
+import craig.software.mc.regen.client.skin.SkinHandler;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractClientPlayer.class)
+public abstract class AbstractClientPlayerMixin {
+
+    @Shadow
+    protected abstract PlayerInfo getPlayerInfo();
+
+    @Inject(at = @At("TAIL"), cancellable = true, method = "getSkinTextureLocation()Lnet/minecraft/resources/ResourceLocation;")
+    protected void getSkinTextureLocation(CallbackInfoReturnable<ResourceLocation> callbackInfoReturnable) {
+        System.out.println(getPlayerInfo());
+        if (SkinHandler.PLAYER_SKINS.containsKey(getPlayerInfo().getProfile().getId())) {
+            ResourceLocation skin = SkinHandler.PLAYER_SKINS.get(getPlayerInfo().getProfile().getId());
+            callbackInfoReturnable.setReturnValue(skin);
+        }
+    }
+}
