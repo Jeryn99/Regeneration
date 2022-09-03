784da3308a1020e45ce0104344d684c582a5d4ec
Lucraft <lucasbandermann@gmail.com>
2022-09-04 00:23:24 +0200

    Forgot this

--- common/src/main/java/mc/craig/software/regen/Regeneration.java
+++ common/src/main/java/mc/craig/software/regen/Regeneration.java
@@ -2,13 +2,16 @@ package mc.craig.software.regen;
 
 import com.mojang.logging.LogUtils;
 import mc.craig.software.regen.client.visual.SkinRetriever;
+import mc.craig.software.regen.network.RegenNetwork;
 import org.slf4j.Logger;
 
 public class Regeneration {
-    public static final String MOD_ID = "regen";
 
+    public static final String MOD_ID = "regen";
     public static final Logger LOGGER = LogUtils.getLogger();
+
     public static void init() {
         SkinRetriever.threadedSetup(false);
+        RegenNetwork.init();
     }
 }
