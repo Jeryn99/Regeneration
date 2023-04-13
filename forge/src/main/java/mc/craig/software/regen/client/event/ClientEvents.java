bdd1d62b3f4bbb7cec26a48f6f252be85c646137
LoxiGoose <60836125+LoxiGoose@users.noreply.github.com>
2023-04-13 11:43:01 -0700

    Keybinds fixed (#429)

---
+++ forge/src/main/java/mc/craig/software/regen/client/event/ClientEvents.java
@@ -0,0 +1,17 @@
+package mc.craig.software.regen.client.event;
+
+import mc.craig.software.regen.Regeneration;
+import mc.craig.software.regen.client.RKeybinds;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Regeneration.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEvents {
+    @SubscribeEvent
+    public static void keyMapping(RegisterKeyMappingsEvent event) {
+        event.register(RKeybinds.FORCE_REGEN);
+        event.register(RKeybinds.REGEN_GUI);
+    }
+}
